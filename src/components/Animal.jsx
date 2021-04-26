4627e580d7edc45f2e53d14f6d45069ec2ae8966
gsong <gsong@DESKTOP-93A0QO2.localdomain>
2021-04-26 20:35:02 +0900

    It is so hard

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 class Animal extends React.Component {
 
-    constructor(props) {
-        super(props);
-        console.log(this.props.id);
+    componentDidMount() {
+        this.getApi();
     }
 
     state = {
-        animal : {}
+        animal : {} 
+    }
+
+    getApi = async () => {
+        const url = `http://acnhapi.com/v1/villagers/${this.props.match.params.id}`;
+        const option = {
+            url : url,
+            method : 'get',
+            headers : {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json;charset=UTF-8'
+            }    
+        }
+
+        try {
+            let api = axios(option);
+            await api.then(this.setAnimalState);
+          } catch (e) {
+            console.error(e);
+        }
+    };
+
+    setAnimalState = (response) => {
+        this.setState({
+            animal : response.data
+        });
+    }
+
+    renderAnimalDom = (animal) => {
+        return(
+            <div style={{ background : animal["bubble-color"] }}>
+                <p style={{ color : animal["text-color"] }}>
+                    No. { animal['id'] } { animal['name']['name-KRko'] }
+                </p>
+                <img alt={animal["image_uri"]} src={ animal["image_uri"] }/>
+            </div> 
+        )
     }
 
     render () {
+        const {animal} = this.state;
+        
         return (
-            <div>this is Animal Detail Page</div>
+            <section>
+                {
+                    animal.id !== undefined
+                    ? this.renderAnimalDom(animal)
+                    : <h1>Loading...</h1>
+                }
+                
+            </section>
         );
     }
 }
